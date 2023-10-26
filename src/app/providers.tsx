5fc6629f2ebf1e5813977dcbc9c3b2a08c770174
matthewpmi <mpmekita@gmail.com>
2023-10-26 15:09:09 -0400

    chore: install next ui, create next ui provider

---
+++ src/app/providers.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { NextUIProvider } from '@nextui-org/react';
+import React from 'react';
+
+export function UIProvider({ children }: {
+  children: React.ReactNode
+}) {
+  return (
+    <NextUIProvider>
+      {children}
+    </NextUIProvider>
+  )
+}
